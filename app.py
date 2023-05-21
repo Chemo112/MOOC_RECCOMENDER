7d3753bc009e89b63869aba84d8b688688af8a0d
chemo <chemotti@gmail.com>
2023-05-21 19:00:02 +0200

    fix errors

--- app.py
+++ app.py
@@ -17,22 +17,30 @@ conn = psycopg2.connect(
     user=db_user,
     password=db_password
 )
-argomenti = ["Programming and software development", "Business and management", "Data science and artificial intelligence", "Engineering and technology", "Art and design", "Social sciences", "Languages and literature", "Mathematics and statistics", "Health sciences", "Physical and natural sciences"]
+argomenti = ["Programming and software development", "Business and management",
+             "Data science and artificial intelligence", "Engineering and technology", "Art and design",
+             "Social sciences", "Languages and literature", "Mathematics and statistics", "Health sciences",
+             "Physical and natural sciences"]
 
 app = Flask(__name__)
 
 app.secret_key = '000999'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'+db_user+':'+db_password+'@'+db_host+':'+db_port+'/'+db_name
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://' + db_user + ':' + db_password + '@' + db_host + ':' + db_port + '/' + db_name
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
     @property
     def is_active(self):
         return True
+
+
 class User_preferences(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topic1 = db.Column(db.Integer, nullable=True)
@@ -52,9 +60,9 @@ class User_preferences(db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
-
     if request.method == 'POST':
         username = request.form['username']
         session['username'] = username
@@ -68,6 +76,7 @@ def login():
             return 'Invalid username or password'
     else:
         return render_template('login.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -100,13 +109,14 @@ def home():
     else:
         return render_template('selezione2.html', argomenti=argomenti)
 
+
 @login_required
 @app.route('/esperienza', methods=['GET', 'POST'])
 def esperienza():
     argomenti = request.args.getlist('argomenti')
     if request.method == 'POST':
         # Gestisci i dati del form in modo appropriato
-        #return redirect(url_for('raccomandazioni2', argomenti=argomenti))
+        # return redirect(url_for('raccomandazioni2', argomenti=argomenti))
         return render_template('esperienza.html', argomenti=argomenti)
 
     elif request.method == 'GET':
@@ -117,9 +127,9 @@ def esperienza():
             # Altrimenti, indirizza l'utente alla pagina raccomandazioni.html
             return render_template('raccomandazioni2.html', argomenti=argomenti)
 
+
 @login_required
 @app.route('/salva-esperienza', methods=['GET'])
-
 def salva_esperienza():
     data = request.args.to_dict()
     topics = [0 for _ in argomenti]
@@ -133,24 +143,24 @@ def salva_esperienza():
 
     for key in data.keys():
         topics[argomenti.index(key)] = int(data[key])
-        new_pref = User_preferences(id=max_id+1,topic1=topics[0],
-                                         topic2=topics[1],
-                                         topic3=topics[2],
-                                         topic4=topics[3],
-                                         topic5=topics[4],
-                                         topic6=topics[5],
-                                         topic7=topics[6],
-                                         topic8=topics[7],
-                                         topic9=topics[8],
-                                         topic10=topics[9],
-                                         username=session['username'])
-    db.add(new_pref)
+        new_pref = User_preferences(id=max_id + 1, topic1=topics[0],
+                                    topic2=topics[1],
+                                    topic3=topics[2],
+                                    topic4=topics[3],
+                                    topic5=topics[4],
+                                    topic6=topics[5],
+                                    topic7=topics[6],
+                                    topic8=topics[7],
+                                    topic9=topics[8],
+                                    topic10=topics[9],
+                                    username=session['username'])
+    db.session.add(new_pref)
     db.session.commit()
 
     return render_template('finale.html')
 
 
-
 import os
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
